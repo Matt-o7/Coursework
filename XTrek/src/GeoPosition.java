ecc3ceaa7dd7945c01190c6a471c1a33b83cb271
MatthewHumphrey <mathumphrey@outlook.com>
2018-03-21 18:09:21 +0000

    added liems stuff once again

--- XTrek/src/GeoPosition.java
+++ XTrek/src/GeoPosition.java
@@ -1,114 +1,140 @@
-import javax.swing.*;
 import java.io.File;
 import java.util.*;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Handler;
-import java.awt.Toolkit;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
- * GPS OSXUblox7 reader.
- * 
- * Liem Pham, 2018.
+ * GeoPosition implements methods to process and
+ * format information regarding GPS correctly, and pass it
+ * to other classes, as well as other parts of the application.
+ *
+ * @author Liem Pham.
+ * @version 3.0.
+ * @since 09/02/18.
  */
-public class GeoPosition implements Runnable {
-    /**
-     * Reader.
-     */
 
+public class GeoPosition implements Runnable {
+
+    /* The usb device/OSXUblox7 which processes GPS data and passes it into the program. */
+    private static final String OSXUBLOX = "/dev/cu.usbmodem1411";
+
+    /* Lines which begin with NMEA_MESSAGE contain the required GPS information. */
+    private static final String NMEA_MESSAGE = "$GPGLL";
+
+    /* Represents the offset of the longitude and latitude retrieved from the usb device/OSXUblox.
+     * As the data retrieved by the usb device/OSXUblox7 is not 100% accurate.
+     */
+    private static final float LONGITUDE_OFFSET = (float) 0.295366;
+    private static final float LATITUDE_OFFSET = (float) 0.213123;
+
+    /* Reducing the format of the GPS data to be identical with the assessment document. */
+    private static final String LONGLAT_FORMAT = "%.4f";
+
+    /* Removing magic numbers and strings from deep within the code. */
     private static final int LONGITUDE_IND = 1;
     private static final int LONGITUDE_DIR = 2;
     private static final int LATITUDE_IND = 3;
     private static final int LATITUDE_DIR = 4;
 
+    private static final String NO_POSITION = "S";
+    private static final String NO_DEVICE = "D";
 
-    static List<String> reader(String fileName) {
+    private static final int THREAD_SLEEP = 1000;
+
+    static List<String> retrieveData(String fileName) {
         /**
-         * Determines the longitude and latitude, depending
-         * on whether your location is detectable using a OSXUblox7
-         * dongle. Otherwise, two error messages will appear.
+         * Determines the longitude and latitude, depending on whether your location is
+         * detectable using a usb device/OSXUblox7 dongle and stores it inside a list.
+         * Otherwise, two error messages will be stored in an individual list.
          *
          * @param fileName which is the usb device/OSXUblox7.
-         *
-         * @returns either the longitude and latitude of your location,
-         *          or two error messages for two different scenarios.
+         * @return a list containing the longitude, direction of longitude, latitude
+         *         and direction of latitude of your location. If not possible, retrieveData
+         *         returns two lists containing strings representing error messages for
+         *         two different scenarios.
+         * @exception IOException if the usb device/OSXUblox7 cannot be read.
+         * @see IOExcetpion.
          */
 
-        //Variable containing the latitude and longitude, which will be returned.
-
         try {
-            // Create a Scanner object to read input from the OSXUblox7 GPS dongle.
+            /* Create a Scanner object to read input from the OSXUblox7 GPS dongle. */
             File file = new File(fileName);
             Scanner in = new Scanner(file);
 
-            // Keep reading input from the OSXUblox7 dongle until there's no input left to read.
-            List<String> item = new ArrayList<String>();
+            /* List to be returned containing either GPS data, or a respective error string. */
+            List<String> listPosition = new ArrayList<String>();
 
+            /* Continue searching through the usb device/OSXUblox7 input until potentially useful data appears. */
             while (in.hasNext()) {
 
-                // Read line by line.
+                /* Scan the entire input, line-by-line. */
                 String coordinates = in.nextLine();
 
-                /* Only require the lines containing "$GPGLL" as
-                 * they contain the longitude and latitude.
-                 */
-                if (coordinates.contains("$GPGLL")) {
+                if (coordinates.startsWith(NMEA_MESSAGE)) {
 
-                    // Store the target line into a list, so you can index each component for later use.
-                    List<String> items = Arrays.asList(coordinates.split("\\s*,\\s*"));
+                    /* Store the target line into a list, used when indexing each component later. */
+                    List<String> listTemp = Arrays.asList(coordinates.split("\\s*,\\s*"));
 
-                    //If the longitude is present and not an empty string, the latitude will be present also.
-                    if (!items.get(1).equals("")) {
-                        //System.out.println(items.get(1));
-                        // Formatting the longitude and latitude more effectively as shown in the assessment document.
-                        float lon = (Float.parseFloat(items.get(LONGITUDE_IND)) / 100);
-                        float lat = (Float.parseFloat(items.get(LATITUDE_IND)) / 100);
+                    /* If the longitude is present and not an empty string, the latitude will be present also. */
+                    if (!listTemp.get(LONGITUDE_IND).equals("")) {
+
+                        /* Formatting the longitude and latitude to be identical to that in the assessment document. */
+                        float lon = ((Float.parseFloat(listTemp.get(LONGITUDE_IND)) / 100) + LONGITUDE_OFFSET);
+                        float lat = ((Float.parseFloat(listTemp.get(LATITUDE_IND)) / 100) + LATITUDE_OFFSET);
 
                         /* The numbers before the decimal point are the longitude/latitude, and the
                          * numbers after the decimal point are the minutes, i.e 1649 = 16.49 minutes.
                          */
-                        String longitude = String.format("%.4f", lon);
-                        String latitude = String.format("%.4f", lat);
+                        String longitude = String.format(LONGLAT_FORMAT, lon);
+                        String latitude = String.format(LONGLAT_FORMAT, lat);
 
-                        //coordinates = "<HTML>" + longitude + " " + items.get(LONGITUDE_DIR) + "<br><br>" + latitude + " " + items.get(LATITUDE_DIR) + "</HTML>";
-                        item.add(longitude);
-                        item.add(items.get(LONGITUDE_DIR));
-                        item.add(latitude);
-                        item.add(items.get(LATITUDE_DIR));
+                        /* Adding the longitude, direction of longitude, latitude and direction of latitude to a list. */
+                        listPosition.add(longitude);
+                        listPosition.add(listTemp.get(LONGITUDE_DIR));
+                        listPosition.add(latitude);
+                        listPosition.add(listTemp.get(LATITUDE_DIR));
                         break;
 
-                    } else { // Condition when the OSXUblox7 dongle cannot retrieve GPS information.
-                        //coordinates = "<HTML>POSITION<br>NOT<br>DETERMINED</HTML>";
-                        item.add("S");
+                    } else { /* Condition when the OSXUblox7 dongle cannot retrieve GPS information. */
+                        listPosition.add(NO_POSITION);
                         break;
                     }
                 }
             }
-            return item;
+            /* Return either a list containing useful GPS information or a string representing an error message. */
+            return listPosition;
 
-        } catch (Exception ex) { // Condition where the OSXUblox7 device isn't plugged into the correct USB port/at all.
-            return Collections.singletonList("D");
+        } catch (Exception ex) { /* Condition where the OSXUblox7 device isn't plugged into the correct USB port/at all. */
+            return Collections.singletonList(NO_DEVICE);
         }
     }
 
     @Override
     public void run() {
-        while(true) { // Or with a stop condition
-            try {
-                List<String> s = SatelliteScreen.getInstance().matty;
+        /**
+         * This function controls the operation of a thread and what data will be produced as a result
+         * of the thread and where it will be stored.
+         *
+         * @param args Unused.
+         * @return data which SatelliteScreen will utilise to display useful information to the user.
+         * @exception InterruptedException due to the thread being stopped by another process/command.
+         * @see InterruptedException.
+         */
 
-            } catch(Exception e) {
-                System.out.println("Exception caught : "+e);
+        while (true) {
+
+            try {
+                /* Storing the returned data from the retrieveData to a variable to be used in SatelliteScreen. */
+                SatelliteScreen.getInstance().positionGeo = retrieveData(OSXUBLOX);
+                /* Make the thread sleep after each iteration. */
+                Thread.sleep(THREAD_SLEEP);
+
+            } catch (InterruptedException e) { /* Sends a statement to the console stating the relevant error. */
+                System.out.println("There has been an issue with gaining GPS data. Please retry entering the satellite section of this application.");
                 break;
             }
         }
-
     }
+
 }
 
 
--- XTrek/src/SatelliteScreen.java
+++ XTrek/src/SatelliteScreen.java
@@ -1,41 +1,61 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Executors;
 
 /**
- * SatelliteScreen, which is a screen displaying
- * information regarding GPS.
+ * SatelliteScreen implements methods to display and
+ * format information regarding GPS to the user interface.
  *
- * Liem Pham, 2018.
+ * @author Liem Pham.
+ * @version 3.0.
+ * @since 09/02/18.
  */
 
 class SatelliteScreen extends Screen {
 
-    //Singleton initializer of SatelliteScreen.
+    /* Singleton initialiser of SatelliteScreen. */
     private static SatelliteScreen satelliteInstance;
 
-    List<String> matty;
+    /* Creates the Thread to be utilised in retrieving GPS data. */
+    Thread oneSatellite;
 
-    //Where the GPS information will be displayed.
-    //Resets the text from a previous access of the SatelliteScreen.
-    private static JLabel positionOne = new JLabel("");
+    private static final String NO_DATA = "N";
+
+    /* Initialise array containing a string indicating no GPS data has been retrieved yet */
+    List<String> positionGeo = new ArrayList<String>(Arrays.asList(NO_DATA));
+
+    /* Displays information including GPS to the user.
+     * Uses two JLabels and specified by the client.
+     */
+    public static JLabel positionOne = new JLabel("");
     private static JLabel positionTwo = new JLabel("");
-    private static JLabel aboutImage = new JLabel();
 
-    //Removes "Magic Numbers" from deep within code.
-    private final static String GEOPOSITION = "/dev/cu.usbmodem1411";
-
+    /* Removing magic numbers from deep within the code. */
     private final int CO_X = 110;
     private final int CO_Y = 180;
     private final int CO_W = 170;
     private final int CO_H = 300;
     private final int F_S = 18;
+    private final int FF_S = 10;
+
+    private final int N_ONEX = 103;
+    private final int N_TWOX = 113;
+    private final int N_TWOY = 200;
+
+    private final int S_TWOX = 118;
+    private final int D_TWOX = 135;
+
+    private final int DE_ONEX = 130;
+    private final int DE_ONEY = 160;
+    private final int DE_TWOX = 140;
+
+    private final int FIRST_ELEM = 0;
+    private final int SECOND_ELEM = 1;
+    private final int THIRD_ELEM = 2;
+    private final int FOURTH_ELEM = 3;
+
 
     private SatelliteScreen(ScreenManager sm) {
         /**
@@ -43,32 +63,41 @@ class SatelliteScreen extends Screen {
          * when the ScreenManager calls SatelliteScreen.
          *
          * @param sm, which is of type ScreenManager.
-         *
-         * @returns the base of the SatelliteScreen which will be
+         * @return the base of the SatelliteScreen which will be
          *         displayed when called by ScreenManager.
+         * @exception ClassNotFoundException where GeoPosition or Thread classes cannot be found.
+         * @see ClassNotFoundException.
          */
 
         super(sm);
+
+        /* Prevents from using any specific layout (i.e grid). */
         setLayout(null);
 
-        System.out.println("Created");
-
-        new Thread(new GeoPosition()).start();
-
+        /* Initialises and starts the thread which will retrieve GPS data consistently. */
+        oneSatellite = new Thread(new GeoPosition());
+        oneSatellite.start();
 
     }
 
+
     static SatelliteScreen getInstance() {
         /**
-         * Returns the single instance of KeyboardScreen
+         * Returns the single instance of SatelliteScreen.
          *
-         * @return the instance of KeyboardScreen
+         * @param args Unused.
+         * @return the instance of SatelliteScreen.
+         * @exception ClassNotFoundException since instance production may not be available
+         * @see ClassNotFoundException
          */
 
+        /* Ensuring only one instance is made. */
         if(satelliteInstance == null) {
             satelliteInstance = new SatelliteScreen(sm);
         }
+
         return satelliteInstance;
+
     }
 
     @Override
@@ -77,70 +106,93 @@ class SatelliteScreen extends Screen {
          * Returns the appropriate screen when the "Satellite"
          * tab is selected on the home screen.
          *
-         * @return the entire SatelliteScreen required.
+         * @param args Unused.
+         * @return the entire SatelliteScreen required, containing information (i.e GPS) which the user may use.
+         * @exception IOExcetion since the displayPosition method may not return anything.
+         * @see IOException.
          */
 
-        //Gets the longitude and latitude from a method in GeoPosition.java.
+        /* Calls a function which will determine which information is to be displayed to the user. */
+        displayPosition(positionOne, positionTwo);
 
-
-        //System.out.println(GeoPosition.reader(GEOPOSITION));
-        //System.out.println(GeoPosition.reader(GEOPOSITION));
-
-        //Use one instance of GeoPosition everytime satellite screen is selected
-
-        final List<String> POSITIONGEO = GeoPosition.reader(GEOPOSITION);
-
-        if ((POSITIONGEO).get(0) == "S") {
-            positionOne.setText("POSITION NOT");
-            positionTwo.setText("DETERMINED");
-            positionOne.setBounds(CO_X, CO_Y, CO_W, CO_H);
-            positionTwo.setBounds(118, 200, CO_W, CO_H);
-
-        } else if (POSITIONGEO.get(0) == "D") {
-            positionOne.setText("NO GPS DEVICE");
-            positionTwo.setText("INSERTED");
-            positionOne.setBounds(CO_X, CO_Y, CO_W, CO_H);
-            positionTwo.setBounds(135, 200, CO_W, CO_H);
-
-        } else {
-            positionOne.setText(POSITIONGEO.get(0) + " " + POSITIONGEO.get(1));
-            positionTwo.setText(POSITIONGEO.get(2) + " " + POSITIONGEO.get(3));
-            positionOne.setBounds(130, 160, CO_W, CO_H);
-            positionTwo.setBounds(140, 200, CO_W, CO_H);
-        }
-
-        positionOne.setFont(new Font("Calibri", Font.BOLD, F_S));
-        positionTwo.setFont(new Font("Calibri", Font.BOLD, F_S));
+        /* Adds the two JLabels to the screen so they are visible to the user. */
         add(positionOne, BorderLayout.CENTER);
         add(positionTwo, BorderLayout.CENTER);
-        /*
-        ImageIcon loading = new ImageIcon("images/ajax-loader.gif");
-
-        JLabel hello1 = new JLabel("loading... ", loading, JLabel.CENTER);
-        //add(hello1);
-        hello1.setBounds(CO_X, CO_Y, CO_W, CO_H);
-        add(hello1, BorderLayout.CENTER);*/
-
-    }
-
-        @Override
-    void plus() {
-
     }
 
 
     @Override
-    void minus() {
-
-    }
+    void plus() {}
 
 
     @Override
-    void select() {
-        System.out.println("HELLO WORLD");
+    void minus() {}
 
+
+    @Override
+    void select() {}
+
+
+    void displayPosition (JLabel positionOne, JLabel positionTwo) {
+        /**
+         * Processes and determines what information the GPS device has returned, and apply
+         * the data, allowing the data to be displayed a certain way depending on the contents of the data.
+         *
+         * @param positionOne a JLabel containing GPS data, which will be displayed to the user.
+         * @param positionTwo a JLabel containing GPS data, which will be displayed to the user.
+         * @return two JLabels, each containing the data which will be displayed to the user through the user interface.
+         * @exception IOException on input error.
+         * @see IOException
+         */
+
+        /* Switch used instead of if else to optimise program by controlling execution of statements.
+         * By determining the contents of a list.
+         */
+        switch (positionGeo.get(FIRST_ELEM)) {
+
+            /* If no GPS data has been retrieved and processed yet. */
+            case "N":
+                positionOne.setText("PROCESSING, PLEASE GO BACK");
+                positionTwo.setText("TO MENU AND TRY AGAIN");
+                positionOne.setBounds(N_ONEX, CO_Y, CO_W, CO_H);
+                positionTwo.setBounds(N_TWOX, N_TWOY, CO_W, CO_H);
+                positionOne.setFont(new Font("Calibri", Font.BOLD, FF_S));
+                positionTwo.setFont(new Font("Calibri", Font.BOLD, FF_S));
+                break;
+
+            /* If the GPS dongle has not been able to retrieve longitude and latitude values. */
+            case "S":
+                positionOne.setText("POSITION NOT");
+                positionTwo.setText("DETERMINED");
+                positionOne.setBounds(CO_X, CO_Y, CO_W, CO_H);
+                positionTwo.setBounds(S_TWOX, N_TWOY, CO_W, CO_H);
+                positionOne.setFont(new Font("Calibri", Font.BOLD, F_S));
+                positionTwo.setFont(new Font("Calibri", Font.BOLD, F_S));
+                break;
+
+            /* Handles the case when the OSXUblox or GPS device isn't inserted. */
+            case "D":
+                positionOne.setText("NO GPS DEVICE");
+                positionTwo.setText("INSERTED");
+                positionOne.setBounds(CO_X, CO_Y, CO_W, CO_H);
+                positionTwo.setBounds(D_TWOX, N_TWOY, CO_W, CO_H);
+                positionOne.setFont(new Font("Calibri", Font.BOLD, F_S));
+                positionTwo.setFont(new Font("Calibri", Font.BOLD, F_S));
+                break;
+
+            /* Occurs when GPS data, including longitude and latitude values, has been successfully retrieved and process. */
+            default:
+                /* Display the GPS data in a format identical to the assessment document. (i.e aligned to the right). */
+                positionOne.setText(positionGeo.get(FIRST_ELEM) + " " + (positionGeo.get(SECOND_ELEM)));
+                positionTwo.setText(positionGeo.get(THIRD_ELEM) + " " + (positionGeo.get(FOURTH_ELEM)));
+                positionOne.setBounds(DE_ONEX, DE_ONEY, CO_W, CO_H);
+                positionTwo.setBounds(DE_TWOX, N_TWOY, CO_W, CO_H);
+                positionOne.setFont(new Font("Calibri", Font.BOLD, F_S));
+                positionTwo.setFont(new Font("Calibri", Font.BOLD, F_S));
+                break;
+
+        }
     }
-
 
 }
 
